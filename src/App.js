20923a6f1bd636a2b5e46584472bc7943cec0a3b
David Barrat <david.barrat.1@gmail.com>
2018-03-16 03:42:00 +0100

    update to webpack4

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello there</p>
+    </div>
+  );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
