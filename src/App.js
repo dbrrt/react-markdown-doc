8c378f2e75156c90b4cc1e4c86b0e91619ed2aeb
David Barrat <david.barrat.1@gmail.com>
2018-03-30 17:30:25 +0200

    testing route feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { Component } from "react";
+import React, { PureComponent, Fragment } from "react";
+import PropTypes from 'prop-types'
 import ReactDOM from "react-dom";
+
+// Router
+import { Router, Switch, Route } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+
+class Hello extends PureComponent {
+  render = () => <Fragment>Hello</Fragment>
+}
+
+class Route2 extends PureComponent {
+  render = () => <Fragment>Route2</Fragment>
+}
+
+
+const history = createBrowserHistory()
 
 const App = () => {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact name='route1' path='/' component={Hello} />
+        <Route exact name='route2' path='/a' component={Route2} />
+      </Switch>
+    </Router>
   );
 };
 
