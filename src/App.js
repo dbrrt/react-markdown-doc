75253af01fc3bd6d780e3ced94a5ea90822d9c16
David Barrat <david.barrat.1@gmail.com>
2018-04-16 18:52:48 +0200

    remove unused assets

--- src/App.js
+++ src/App.js
@@ -2,31 +2,19 @@
 import React, { PureComponent, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
-
-// Router
 import { Router, Switch, Route, Link } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
-
-import { NotFound } from './NotFound'
 import { routes } from '~/config'
-
-type Props = {};
-type State = {};
-
-const history = createBrowserHistory()
-
+// $FlowFixMe
 import './theme/base.scss'
-
-const App = () => {
-  return (
-    <Router history={history}>
+// MainApp
+const App = () => (
+    <Router history={createBrowserHistory()}>
       <Switch>
         {routes.map((el: any, i: number) => (
           <Route exact name={i} key={i} path={el.path} component={el.component} />
         ))}
       </Switch>
     </Router>
-  );
-};
-
+)
 export { App }
