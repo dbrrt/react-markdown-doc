01f7f461f2fdd67da9af7f05c9724d73ed4ccaae
David Barrat <david.barrat@protonmail.com>
2018-05-01 10:16:19 +0200

    Update utils.js

--- config/utils.js
+++ config/utils.js
@@ -2,4 +2,4 @@ import React from 'react'
 import { GenericView } from '~/src/GenericView'
 const passProps = injectedProps => Wrapped => props => <Wrapped {...injectedProps} {...props} />
 const composeMD = md => passProps({ md })(GenericView)
-export { passProps, composeMD }
+export { composeMD }
